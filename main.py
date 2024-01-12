01a320bac0f4ccb373112dd56aa537ce7e167e77
Soulbolt <soulbolt@gmail.com>
2024-01-12 00:36:29 -0700

    implemented code to capture form data and send formated email. modified some code so that success message appears on contact html image in place of "conctact" h1 header

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
 
+
+MY_EMAIL = "EMAIL GOES HERE"
+MY_PASSWORD = "PASSWORD HERE"
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
@@ -20,7 +24,17 @@ def about():
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        return "<h1>Successfully sent your message.</h1>"
+        success = "Successfully sent your message."
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:User Message!\n\nName: {request.form.get('name')\nEmail: {request.form.get('email')} \
+            \nPhone: {request.form.get('phone')}\nMessage: {request.form.get('message')}"
+        )
+        return render_template("contact.html", confirm=success)
     else:
         return render_template("contact.html")
 
